931ed405c166e655132cbb53e7d53764e5aff14b
Sarveshsingh09 <sarvesh8882589477@gmail.com>
2024-08-13 21:09:54 +0530

    init

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { Editor } from '@/components/editor/Editor'
+import React from 'react'
+
+const Documents = () => {
+  return (
+    <Editor/>
+  )
+}
+
+export default Documents
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Documents = () => {
+  return (
+    <div>home</div>
+  )
+}
+
+export default Documents
