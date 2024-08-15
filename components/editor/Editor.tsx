7bf11e7c47bb06bf10ee991c7004fb5cd9837715
Sarveshsingh09 <sarvesh8882589477@gmail.com>
2024-08-15 11:22:41 +0530

    new

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -17,10 +17,6 @@ import Loader from '../Loader';
 import FloatingToolbarPlugin from './plugins/FloatingToolbarPlugin'
 import { useThreads } from '@liveblocks/react/suspense';
 
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
-
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
 }
@@ -45,7 +41,7 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
       <div className="editor-container size-full">
         <div className="toolbar-wrapper flex min-w-full justify-between">
           <ToolbarPlugin />
-         
+          {/* {currentUserType === 'editor' && <DeleteModal roomId={roomId} />} */}
         </div>
 
         <div className="editor-wrapper flex flex-col items-center justify-start">
@@ -67,7 +63,7 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
           <LiveblocksPlugin>
             <FloatingComposer className="w-[350px]" />
             <FloatingThreads threads={threads} />
-          
+            {/* <Comments /> */}
           </LiveblocksPlugin>
         </div>
       </div>
